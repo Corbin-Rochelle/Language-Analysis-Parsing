e8d540dfed1591c9a158216e70910d0aef5db6c8
Corbin-Rochelle <ctr233@msstate.edu>
2022-10-26 20:43:06 -0500

    uususu

--- Part-2-Starting_Point/1-hello.pas
+++
@@ -1,5 +0,0 @@
-PROGRAM HELLO;
-{ The TIPS Hello World program! }
-BEGIN
-    WRITE('Hello World!')
-END
---
+++ Part-2/parser.h
@@ -0,0 +1,36 @@
+//**************************************************************************
+ 
+// Replace with appropriate header comment......
+
+//**************************************************************************
+
+#ifndef PARSER_H
+#define PARSER_H
+
+#include <string>
+#include <set>
+#include <iostream>
+
+using namespace std;
+
+// Holds the symbols in the interpreted program
+extern set<string> symbolTable;
+
+extern int nextToken;        // next token returned by lexer
+
+extern "C" {
+	// Instantiate global variables used by flex
+	extern int   yylex();      // the generated lexical analyzer
+	extern char* yytext;       // text of current lexeme
+}
+
+// Function declarations
+void program();
+void block();
+void statement();
+void expression();
+void simple_expression();
+void term();
+void factor();
+
+#endif /* PARSER_H */
